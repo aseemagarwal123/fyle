fcea3d8039c4741fb2a6ec6f64e8509b75c5a2e7
aseemagarwal123 <aseem7570@gmail.com>
2019-11-20 23:47:25 +0530

    auth changes

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,9 +4,7 @@ module.exports=function auth(req, res, next) {
 try {
     const token=req.header("x-auth-token");
     if (!token) return res.status(401).send("Access denied. No token provided");
-    const decoded=jwt.verify(token, "secretkey");
-    const type=decoded.type;
-    if(type!='user') return res.status(401).send("Access denied. You are not an authenticated user");
+
     next();
   } catch (ex) {
     res.status(400).send(ex);
