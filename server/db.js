920d6fd81b03d63de0b002c1b5de0838d25ae5d9
aseemagarwal123 <aseem7570@gmail.com>
2019-11-26 13:22:19 +0530

    added vars

--- server/db.js
+++ server/db.js
@@ -10,6 +10,5 @@ client.connect().then((d)=>{
 console.log('postgres connected')
 
 });
-//postgres://hekgtrihsegliv:637b110c5282d55babc730f376ae50a81be42798ead926f4c8ed42db1503d859@ec2-107-22-253-158.compute-1.amazonaws.com:5432/d5qua9egs8nscc?sslmode=disable
 
 exports.client = client;
